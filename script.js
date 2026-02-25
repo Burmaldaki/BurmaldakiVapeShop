29babc7e4f9e82dd94462cf819766e71e05a691c
Burmaldaki <yarikgorodiskiy1908@gmail.com>
2026-02-25 23:17:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const tg = window.Telegram.WebApp;
-tg.expand();
-function showSettings() { document.getElementById('catalog').style.display='none'; document.getElementById('settings').style.display='block'; }
-function hideSettings() { document.getElementById('catalog').style.display='block'; document.getElementById('settings').style.display='none'; }
+
